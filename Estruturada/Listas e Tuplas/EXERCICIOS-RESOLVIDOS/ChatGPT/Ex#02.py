959026858b1a307e867476f4f20cee2730a72d97
Israel Alves <alves.israel@aluno.ifce.edu.br>
2024-12-21 17:42:20 -0300

    Rename Ex#2.py to Ex#02.py

---
+++ Estruturada/Listas e Tuplas/EXERCICIOS-RESOLVIDOS/ChatGPT/Ex#02.py
@@ -0,0 +1,5 @@
+#Exercício #2
+
+myTuple = (1,2,3,4, "cachorro")
+
+print(f"O último elemento da tupla é {myTuple[-1]}")
--- Estruturada/Listas e Tuplas/EXERCICIOS-RESOLVIDOS/ChatGPT/Ex#2.py
+++
@@ -1,5 +0,0 @@
-#Exercício #2
-
-myTuple = (1,2,3,4, "cachorro")
-
-print(f"O último elemento da tupla é {myTuple[-1]}")
