f08605082432fe153ad4f5865a8ebf1fd38d6e1d
Israel Alves <alves.israel@aluno.ifce.edu.br>
2024-12-21 17:44:11 -0300

    Rename Ex#7.py to Ex#07.py

---
+++ Estruturada/Listas e Tuplas/EXERCICIOS-RESOLVIDOS/ChatGPT/Ex#07.py
@@ -0,0 +1,6 @@
+#Exercício #7
+
+matriz = [[1,2],
+          [3,4]]
+
+print(f"Elemento da primeira linha e segunda coluna: {matriz[0][1]}")
--- Estruturada/Listas e Tuplas/EXERCICIOS-RESOLVIDOS/ChatGPT/Ex#7.py
+++
@@ -1,6 +0,0 @@
-#Exercício #7
-
-matriz = [[1,2],
-          [3,4]]
-
-print(f"Elemento da primeira linha e segunda coluna: {matriz[0][1]}")
